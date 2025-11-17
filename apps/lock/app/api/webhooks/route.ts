aeee3f5fbd2efc56c56c70cfd6040be91add720f
Nathan Le Granvallet <nathan@legranvallet.fr>
2025-11-18 00:21:37 +0100

    Handle webhooks

--- apps/lock/app/api/webhooks/route.ts
+++ apps/lock/app/api/webhooks/route.ts
@@ -1,50 +1,100 @@
 import { APIResponse } from "@/app/utils/api/actions";
-import { withAuth } from "@/app/utils/api/auth";
 import { prisma } from "@repo/db";
 import { NextRequest } from "next/server";
-import { z, ZodError } from "zod";
+import crypto from "crypto";
 
-const schema = z.object({
-  requestId: z.string().min(1, "Request ID is required"),
-  success: z.boolean(),
-  errorCode: z.string().optional(),
-});
+export async function POST(req: NextRequest) {
+  const rawBody = await req.text();
 
-async function securePOST(req: NextRequest) {
-  const formData = await req.formData();
+  const hmac = crypto
+    .createHmac("sha256", process.env.NUKI_CLIENT_SECRET)
+    .update(rawBody, "utf8")
+    .digest("hex");
 
-  const data = Object.fromEntries(formData);
-  const parsed = schema.safeParse(data);
+  const signature = req.headers.get("X-Nuki-Signature-SHA256");
 
-  if (parsed.success) {
+  if (signature !== hmac) {
+    return APIResponse({ error: { message: "Invalid signature" } }, 401);
+  }
+
+  let parsed;
+
+  try {
+    parsed = JSON.parse(rawBody);
+  } catch {
+    return APIResponse({ error: { message: "Request body is invalid" } }, 422);
+  }
+
+  if (!parsed.smartlockId) {
+    return APIResponse({ error: { message: "smartlockId is required" } }, 400);
+  }
+
+  let lock;
+
+  try {
+    lock = await prisma.lock.findFirstOrThrow({
+      where: {
+        nukiId: parsed.smartlockId.toString(),
+      },
+    });
+  } catch {
+    return APIResponse({ error: { message: "Lock cannot be found" } }, 400);
+  }
+
+  if (parsed.feature === "DEVICE_STATUS") {
+    const reqFromLockCrfNotLongAgo = await prisma.request.count({
+      where: {
+        lockId: lock.id,
+        createdAt: {
+          gt: new Date(Date.now() - 10 * 1000), // 10 seconds ago
+        },
+      },
+    });
+
+    if (reqFromLockCrfNotLongAgo === 0) {
+      if (
+        parsed.state?.state === 1 ||
+        parsed.state?.state === 3 ||
+        parsed.state?.state === 254
+      ) {
+        await prisma.log.create({
+          data: {
+            lock: { connect: { id: lock.id } },
+            action: parsed.state?.state,
+            details: JSON.stringify(parsed),
+          },
+        });
+      }
+
+      return APIResponse({ message: "Log added successfully" }, 200);
+    } else {
+      return APIResponse(
+        { message: "Log was not added since the request was made by lock.crf" },
+        200,
+      );
+    }
+  }
+
+  if (parsed.requestId) {
     try {
       await prisma.request.update({
-        data: {
-          success: parsed.data.success,
-          error: parsed.data.errorCode || null,
-        },
         where: {
-          id: parsed.data.requestId,
+          id: parsed.requestId.toString(),
+        },
+        data: {
+          success: parsed.success || false,
+          error: parsed.errorCode || null,
         },
       });
 
       return APIResponse({ message: "Request updated successfully" }, 200);
     } catch {
       return APIResponse(
-        { error: { message: "Request cannot be found" } },
-        400,
+        { error: { message: "Failed to update the request status" } },
+        500,
       );
     }
-  } else {
-    const error: ZodError = parsed.error;
-    let errorMessage = "";
-
-    error.issues.map((issue) => {
-      errorMessage += issue.message + "\n";
-    });
-
-    return APIResponse({ error: { message: errorMessage } }, 400);
   }
-}
 
-export const POST = withAuth(securePOST);
+  return APIResponse({ message: "Unknown action" }, 400);
+}
